c5e38ea351acb8c5fecf90f4f0eba11e6ef7a35d
Damien38340 <damien.lobato@le-campus-numerique.fr>
2024-10-03 16:10:23 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Character {
+
+    private String name;
+    private String type;
+
+public Character() {
+
+}
+
+public Character(String name) {
+this.name = name;
+}
+
+public Character(String name, String type) {
+this.name = name;
+this.type = type;
+}
+
+
+    public static void main(String[] args) {
+
+    }
+}
